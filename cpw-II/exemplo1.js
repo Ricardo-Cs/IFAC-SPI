4b276fb1a08c93f94071fa352d7bcd2feb226a83
Ricardo <rcsilva2405@gmail.com>
2025-03-31 17:40:44 -0500

    Aula dia 31/03 - CPW-II

--- cpw-II/exemplo1.js
+++ cpw-II/exemplo1.js
@@ -1,9 +1,25 @@
+const p1 = document.getElementById("p1");
+const p2 = document.getElementById("p2");
+
 function voltar() {
     history.back();
-    console.log("voltar");
 }
 
 function avancar() {
     history.forward();
-    console.log("avançar");
+}
+
+function alterar() {
+    if (p1.innerHTML == "Novo Texto") {
+        p1.innerHTML = "Primeiro exemplo JS";
+        p1.style.color = "blue";
+    }
+    else {
+        p1.innerHTML = "Novo Texto";
+        p1.style.color = "red";
+    }
+}
+
+function remover() {
+    p2.style.visibility == "visible" ? p2.style.visibility = "hidden" : p2.style.visibility = "visible";
 }
