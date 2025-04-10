afc5dfbd03104006fa4f298055fd79fe77973748
Ricardo-Cs <rcsilva2405@gmail.com>
2025-04-10 15:30:09 -0500

    Questão 4 e 5 de CPW lista 1

--- CPW-II/atividades/lista-1/lista.js
+++ CPW-II/atividades/lista-1/lista.js
@@ -13,6 +13,25 @@ function calculoIMC() {
 }
 
 function calcularMedia() {
-    const notas = document.getElementsByName("nota")[0].value;
-    console.log(notas);
+    const notas = document.getElementsByName("nota");
+    const mediaResposta = document.getElementById("media-resposta");
+    let media = 0;
+    notas.forEach(nota => {
+        media += parseInt(nota.value);
+    });
+    mediaResposta.innerText = `A média das notas é: ${media / 4}`;
+}
+
+function maiorNumero() {
+    const numeros = document.getElementById("maior-numero").value.split(",");
+    const maiorResposta = document.getElementById("maior-resposta");
+    maiorResposta.innerText = `O maior número é: ${Math.max(...numeros)}`;
+}
+
+function inverterPalavra() {
+    const palavra = document.getElementById("palavra-invertida").value;
+    // Split transforma a palavra em um array, com os elementos sendo cada letra. Reverse inverte os valores desse array. Join junta tudo e retorna uma string.
+    const invertida = palavra.split('').reverse().join(''); 
+    const inverterResposta = document.getElementById("palavra-invertida-resposta");
+    inverterResposta.innerText = `A palavra ${palavra} ao contrário é: ${invertida}`;
 }
