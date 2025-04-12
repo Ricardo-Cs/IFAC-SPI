7e6cf0fce90eb93794ea5d547f529e1200209be2
Ricardo Costa <106176120+Ricardo-Cs@users.noreply.github.com>
2025-04-12 08:01:26 -0500

    Update acelerador.java

--- orientacao-a-objetos/exercicios/acelerador.java
+++ orientacao-a-objetos/exercicios/acelerador.java
@@ -6,10 +6,8 @@ public class acelerador
 	public static void main(String[] args) {
 		Scanner s = new Scanner(System.in);
 		int D = s.nextInt() - 5;
-		int resultado = D % 8;
-		
-		if (resultado == 1) System.out.println(1);
-		else if (resultado == 2) System.out.println(2);
-		else System.out.println(3);
+		D %= 8;
+
+		System.out.println(D);
 	}
 }
