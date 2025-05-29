54869db40448f23f5d83787e25062d8334a5e5d1
Ricardo-Cs <rcsilva2405@gmail.com>
2025-05-29 14:09:50 -0500

    Lista 3 CPW-II parcial

--- cpw-II/atividades/lista-4/lista.js
+++ cpw-II/atividades/lista-4/lista.js
@@ -1,7 +1,7 @@
 function validarData() {
     const data = document.getElementById('data').value;
     const dataResposta = document.getElementById('dataResposta');
-    const regexData = /((0[1-9])|((1|2)[0-9])|(3[01])|([1-9]{1}))\/(([1-9])|(0[1-9])|(1[012]))\/([0-9]{4})/g;
+    const regexData = /^((0[1-9])|((1|2)[0-9])|(3[01])|([1-9]{1}))\/(([1-9])|(0[1-9])|(1[012]))\/([0-9]{4})/g;
 
     const resultado = regexData.exec(data);
     if(resultado) {
@@ -10,5 +10,35 @@ function validarData() {
     } else {
         dataResposta.innerText = "Data inválida";
         dataResposta.style.color = "red";
+    }
+}
+
+function validarPlaca() {
+    const placa = document.getElementById('placa').value;
+    const placaResposta = document.getElementById('placaResposta');
+    const regexPlaca = /[A-Z]{3}[0-9][A-Z][0-9]{2}/g;
+
+    const resultado = regexPlaca.exec(placa);
+    if(resultado) {
+        placaResposta.innerText = "Placa válida!";
+        placaResposta.style.color = "green";
+    } else {
+        placaResposta.innerText = "Placa inválida!";
+        placaResposta.style.color = "red";
+    }
+}
+
+function validarCPF() {
+    const cpf = document.getElementById('cpf').value;
+    const cpfResposta = document.getElementById('cpfResposta');
+    const regexCpf = /[0-9]{3}\.[0-9]{3}\.[0-9]{3}\-[0-9]{2}/g;
+
+    const resultado = regexCpf.exec(cpf);
+    if(resultado) {
+        cpfResposta.innerText = "CPF válido!";
+        cpfResposta.style.color = "green";
+    } else {
+        cpfResposta.innerText = "CPF inválido!";
+        cpfResposta.style.color = "red";
     }
 }
